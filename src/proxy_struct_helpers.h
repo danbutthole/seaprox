51f8da5e34944e6e37208bc040fe639690e83c36
Dan Farrell <partybutthole@gmail.com>
2021-09-24 11:08:32 +1000

    helpers: proxy struct helpers
    Added defs for getting around proxy structures.

---
+++ src/proxy_struct_helpers.h
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2021 - 2021, Dan Farrell and the seaprox contributors
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#include "proxy_connection.h"
+#include "proxy_side.h"
+#include "utils_ptr.h"
+
+#ifndef PROXY_STRUCT_HELPERS_H
+#define PROXY_STRUCT_HELPERS_H
+
+struct proxy_connection* proxy_side_get_connection(
+	struct proxy_side *side);
+
+struct proxy_side* proxy_side_get_other_side(
+	struct proxy_side *side);
+
+struct proxy_side* proxy_data_get_side(void *data);
+
+#define /* PROXY_STRUCT_HELPERS_H */
