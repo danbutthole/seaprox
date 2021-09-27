20af463be8e46e22ad70412f774cf6f489814317
Dan Farrell <partybutthole@gmail.com>
2021-09-27 12:10:56 +1000

    scripts: check headers: checks just headers in check headeers script
    Read heading.

--- scripts/check_headers.sh
+++ scripts/check_headers.sh
@@ -4,8 +4,8 @@ HERE=$(dirname ${0})
 
 # Check with gcc
 make -C "${HERE}/../src/" clean
-make CC=gcc -C "${HERE}/../src/"
+make CC=gcc -C "${HERE}/../src/" headers
 
 # Check with clang
 make -C "${HERE}/../src/" clean
-make CC=clang -C "${HERE}/../src/"
+make CC=clang -C "${HERE}/../src/" headers
